38e9a4b1d8ca95085517e9fd02dafbe43eba22d0
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-04-09 03:04:00 +0530

    added axios may remove it later

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const url = document.querySelector('#url');
 const timestampInput = document.querySelector('#timestamp');
+const notesForm = document.querySelector('#notesForm');
 
 const code = `(function getTimeStamp(){
     return document.querySelector('span.ytp-time-current').innerText
@@ -26,3 +27,35 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   );
 });
+
+notesForm.onsubmit = (e) => {
+  e.preventDefault();
+  axios
+    .post('http://localhost:3001/api/notes', {
+      url: e.target.url.value,
+      timestamp: e.target.timestamp.value,
+      note: e.target.note.value,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  // const response = await fetch('http://localhost:3001/api/notes', {
+  //   method: 'POST',
+  //   body: {
+  //     url: e.target.url.value,
+  //     timestamp: e.target.timestamp.value,
+  //     note: e.target.note.value,
+  //   },
+  // });
+
+  // try {
+  //   const result = await response.json();
+  //   console.log(result);
+  // } catch (error) {
+  //   console.log(error);
+  // }
+};
